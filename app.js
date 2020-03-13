45ba380ed2cc8134051f31d44b218dd23727f54d
rahul26021999 <rahul26021999@gmail.com>
2020-03-13 14:13:17 +0530

    manage.js added

--- app.js
+++ app.js
@@ -12,11 +12,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 // const adminData = require('./routes/admin');
 // const shopRoutes = require('./routes/shop');
 
+app.use('/manage', (req, res, next) => {
+    console.log('in maane middle');
+    res.sendFile(path.join(__dirname, 'views', 'manage.html'));
+});
 app.use('/', (req, res, next) => {
     console.log('in another middle');
     res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
-
 app.use((req, res, next) => {
     res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
